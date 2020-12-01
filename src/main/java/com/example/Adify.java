3ed5cb02dbdb355af768b856cad38e0014eff146
Niilo Korkiakoski <niilo.korkiakoski@eficode.com>
2020-12-01 16:43:06 +0200

    feature flag

--- src/main/java/com/example/Adify.java
+++ src/main/java/com/example/Adify.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import com.optimizely.ab.Optimizely;
+import com.optimizely.ab.OptimizelyFactory;
 
 class Adify {
   private final ExternalService service;
@@ -11,10 +13,17 @@ class Adify {
   }
 
   String fetch(String productId) {
+    String sdkKey = "WYEjq8y2NG46HFWxmyMjJ";
+    Optimizely optimizelyClient = OptimizelyFactory.newDefaultInstance(sdkKey);
+    Boolean enabled = optimizelyClient.isFeatureEnabled("Product-name", Math.random()+"");
     try {
       String content = service.get("?product=" + productId);
       JSONObject obj = (JSONObject) new JSONParser().parse(content);
-      return (String) obj.get("product");
+      if (enabled) {
+        return (String) obj.get("product-name");
+      } else {
+        return (String) obj.get("product");
+      }
     } catch (Exception e) {
       return "";
     }
--- src/test/java/com/example/AdifyServiceTest.java
+++ src/test/java/com/example/AdifyServiceTest.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,12 +11,19 @@ class AdifyServiceTest {
 
   class SenderSpy implements Subscription.Sender {
     String event;
+    String body;
 
     @Override
     public void send(String event, String body) {
       this.event = event;
+      this.body = body;
     }
   }
+
+  class Stub implements ExternalService {
+    public String get(String arg) throws IOException{return "{\"product\":\"Paper\"}";}
+  }
+
 
   @Test
   @Tag("slow")
@@ -22,15 +31,46 @@ class AdifyServiceTest {
     SenderSpy spy = new SenderSpy();
     AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), "SESSION_ID,USER_ID,PRODUCT_ID", spy);
     a.execute();
-    assertEquals("fixme", spy.event);
+    assertEquals("display", spy.event);
+    System.out.println(spy.body);
+    String[] values = spy.body.split(",");
+    assertEquals("SESSION_ID", values[0]);
+    assertEquals("advert", values[1]);
+    assertEquals("PRODUCT_ID", values[2]);
+    assertEquals("PRODUCT_NAME", values[3]);
   }
 
+//component test
   @Test
-  @Tag("slow")
-  void bar() {
+  @Tag("fast")
+  void component_test() {
     SenderSpy spy = new SenderSpy();
-    AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), "SESSION_ID,advert,PRODUCT_ID,PRODUCT_NAME", spy);
+    AdifyService a = new AdifyService(new Adify(new Stub()), "SESSION_ID,USER_ID,PRODUCT_ID", spy);
     a.execute();
     assertEquals("display", spy.event);
-}
+    System.out.println(spy.body);
+    String[] values = spy.body.split(",");
+    assertEquals("SESSION_ID", values[0]);
+    assertEquals("advert", values[1]);
+    assertEquals("PRODUCT_ID", values[2]);
+    assertEquals("Paper", values[3]);
+  }
+
+// integration test
+  @Test
+  @Tag("slow")
+  void integration_test() throws Exception {
+    Adify a = new Adify(new HerokuGetRequest("adify"));
+    String result = a.fetch("PRODUCT_ID");
+    assertEquals("Paper", result);
+  }
+
+// contract test
+  @Test
+  @Tag("slow")
+  void contract_test() throws Exception {
+    HerokuGetRequest a = new HerokuGetRequest("adify");
+    String content = a.get("?product=" + "PRODUCT_ID");
+    assertEquals("{\"product\":\"Paper\"}", content);
+  }
 }
